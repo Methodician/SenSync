f90b385afc705c701fee3629e91fc754a52fef91
Jacob Johnston <methodician@gmail.com>
2022-01-31 19:15:06 +0000

    Functions keeps list up to date

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -9,8 +9,6 @@ module.exports = {
     "plugin:import/errors",
     "plugin:import/warnings",
     "plugin:import/typescript",
-    "google",
-    "plugin:@typescript-eslint/recommended",
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
@@ -20,12 +18,8 @@ module.exports = {
   ignorePatterns: [
     "/lib/**/*", // Ignore built files.
   ],
-  plugins: [
-    "@typescript-eslint",
-    "import",
-  ],
+  plugins: ["@typescript-eslint", "import"],
   rules: {
-    "quotes": ["error", "double"],
     "import/no-unresolved": 0,
   },
 };
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,4 @@
-import * as functions from "firebase-functions";
+import * as functions from 'firebase-functions';
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -7,3 +7,34 @@ import * as functions from "firebase-functions";
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+export const onFirstReadout = functions.database
+  .ref('/readoutsByModuleId/{moduleId}')
+  .onCreate((snapshot, context) => {
+    const val = snapshot.val();
+    const { moduleId } = context.params;
+    const seedModule = {
+      name: 'UNAMED MODULE',
+      lastReadout: val[Object.keys(val)[0]],
+    };
+    functions.logger.log('Initializing module');
+    return snapshot.ref.parent?.parent
+      ?.child('modules')
+      .child(moduleId)
+      .set(seedModule);
+  });
+
+export const onNewReadout = functions.database
+  .ref('/readoutsByModuleId/{moduleId}/{readoutId}')
+  .onCreate((snapshot, context) => {
+    const val = snapshot.val();
+    const { moduleId } = context.params;
+    const moduleUpdate = {
+      lastReadout: val,
+    };
+    functions.logger.log('Updating module');
+    return snapshot.ref.parent?.parent?.parent
+      ?.child('modules')
+      .child(moduleId)
+      .update(moduleUpdate);
+  });
