14964bc3187607c5397d70c2ba9fab103daf7391
Methodician <methodician@gmail.com>
2022-01-30 16:35:44 -0800

    Ng Added NgFire

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'senspout',
+    appId: '1:561195124727:web:77d7a7fa07fbd137ac50f2',
+    databaseURL: 'https://senspout-default-rtdb.firebaseio.com',
+    storageBucket: 'senspout.appspot.com',
+    apiKey: 'AIzaSyAOacMZObUL6SBQE0PvUp9oxtm39W-E2bQ',
+    authDomain: 'senspout.firebaseapp.com',
+    messagingSenderId: '561195124727',
+  },
   production: false
 };
 
