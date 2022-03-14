f3bc5a0969a6b4541068202879682188ece6cc5a
Jacob Johnston <methodician@gmail.com>
2022-03-14 17:52:36 +0000

    Basic working with alt update uptions

--- src/app/components/module-overview/module-overview.component.ts
+++ src/app/components/module-overview/module-overview.component.ts
@@ -7,8 +7,54 @@ import {
   YAXisComponentOption,
   SeriesOption,
 } from 'echarts';
-import { combineLatest, map, Observable, switchMap } from 'rxjs';
+import { combineLatest, first, map } from 'rxjs';
 import { ModuleI, ReadoutI } from 'src/app/models';
+
+const baseChatOption: EChartsOption = {
+  // toolbox: {
+  //   right: 20,
+  //   feature: {
+  //     dataZoom: {
+  //       yAxisIndex: 'none',
+  //     },
+  //     saveAsImage: {},
+  //     restore: {},
+  //     magicType: {},
+  //   },
+  // },
+  tooltip: {
+    trigger: 'axis',
+    axisPointer: {
+      type: 'cross',
+    },
+  },
+  legend: {},
+  xAxis: {
+    type: 'time',
+    axisLabel: {
+      formatter: function (value: number) {
+        let date = new Date(value);
+        if (date.getHours() === 0) {
+          return date.toLocaleDateString();
+        }
+
+        return date.toLocaleTimeString();
+      },
+    },
+  },
+  yAxis: {
+    type: 'value',
+    name: 'y axis',
+    min: 12,
+  },
+  series: [],
+  dataZoom: [
+    {
+      startValue: new Date().setHours(0),
+    },
+    { type: 'inside' },
+  ],
+};
 
 @Component({
   selector: 'sen-module-overview',
@@ -17,88 +63,98 @@ import { ModuleI, ReadoutI } from 'src/app/models';
 })
 export class ModuleOverviewComponent {
   //! querying is sooo bad with rtdb maybe I really should just go with firestore
-  chartOption$: Observable<EChartsOption>;
+  chartOption: EChartsOption = baseChatOption;
+  updateOption: EChartsOption;
 
   constructor(
     private activeRoute: ActivatedRoute,
     private db: AngularFireDatabase,
   ) {
-    this.chartOption$ = this.activeRoute.params.pipe(
-      map(params => params['id']),
-      switchMap((id: string) => {
+    this.initializeChart();
+  }
+
+  initializeChart = () => {
+    this.activeRoute.params
+      .pipe(
+        map(params => params['id']),
+        first(id => !!id),
+      )
+      .subscribe(id => {
         const module$ = this.db.object<ModuleI>(`modules/${id}`).valueChanges();
         const readouts$ = this.db
           .list<ReadoutI>('readouts', ref =>
             ref.orderByChild('moduleId').equalTo(id),
           )
           .valueChanges();
-        return combineLatest([module$, readouts$]);
-      }),
-      map(([module, readouts]) => {
-        if (!readouts) {
-          return {};
-        }
-        const xAxis: XAXisComponentOption = {
-          type: 'time',
-          alignTicks: true,
-          axisLabel: {
-            formatter: function (value: number) {
-              let date = new Date(value);
-              if (date.getHours() === 0) {
-                return date.toLocaleDateString();
-              }
+        combineLatest([module$, readouts$]).subscribe(([module, readouts]) => {
+          if (!readouts) {
+            return;
+          }
+          const xAxis: XAXisComponentOption = {
+            type: 'time',
+            alignTicks: true,
+            axisLabel: {
+              formatter: function (value: number) {
+                let date = new Date(value);
+                if (date.getHours() === 0) {
+                  return date.toLocaleDateString();
+                }
 
-              return date.toLocaleTimeString();
+                return date.toLocaleTimeString();
+              },
             },
-          },
-        };
-        const humidity = readouts.map(readout => [
-          readout.timestamp,
-          readout.bme.humidity,
-        ]);
-        const temperature = readouts.map(readout => [
-          readout.timestamp,
-          readout.bme.temperature,
-        ]);
-        const yAxisHumidity: YAXisComponentOption = {
-          type: 'value',
-          name: 'Humidity',
-        };
-        const yAxisTemperature: YAXisComponentOption = {
-          name: 'Temperature',
-          alignTicks: true,
-          type: 'value',
-        };
-        const yAxis: YAXisComponentOption[] = [yAxisHumidity, yAxisTemperature];
-        const humiditySeries: SeriesOption = {
-          name: 'Humidity',
-          type: 'line',
-          data: humidity,
-        };
-        const temperatureSeries: SeriesOption = {
-          name: 'Temperature',
-          type: 'line',
-          data: temperature,
-          yAxisIndex: 1,
-        };
-        const series: SeriesOption[] = [humiditySeries, temperatureSeries];
-        const chartOption: EChartsOption = {
-          title: {
-            text: `${module?.name} - Humidity vs Temperature`,
-            left: 'center',
-          },
-          tooltip: {
-            trigger: 'axis',
-            axisPointer: {
-              type: 'cross',
+          };
+          const humidity = readouts.map(readout => [
+            readout.timestamp,
+            readout.bme.humidity,
+          ]);
+          const temperature = readouts.map(readout => [
+            readout.timestamp,
+            readout.bme.temperature,
+          ]);
+          const yAxisHumidity: YAXisComponentOption = {
+            type: 'value',
+            name: 'Humidity',
+          };
+          const yAxisTemperature: YAXisComponentOption = {
+            name: 'Temperature',
+            alignTicks: true,
+            type: 'value',
+          };
+          const yAxis: YAXisComponentOption[] = [
+            yAxisHumidity,
+            yAxisTemperature,
+          ];
+          const humiditySeries: SeriesOption = {
+            name: 'Humidity',
+            type: 'line',
+            data: humidity,
+          };
+          const temperatureSeries: SeriesOption = {
+            name: 'Temperature',
+            type: 'line',
+            data: temperature,
+            yAxisIndex: 1,
+          };
+          const series: SeriesOption[] = [humiditySeries, temperatureSeries];
+          const chartOption: EChartsOption = {
+            title: {
+              text: `${module?.name} - Humidity vs Temperature`,
+              left: 'center',
+              top: 20,
             },
-          },
-          yAxis,
-          xAxis,
-          series,
-        };
-        return chartOption;
-      }),
-    );
-  }
+            tooltip: {
+              trigger: 'axis',
+              axisPointer: {
+                type: 'cross',
+              },
+            },
+            yAxis,
+            xAxis,
+            series,
+          };
+          this.updateOption = chartOption;
+        });
+      });
+  };
 }
