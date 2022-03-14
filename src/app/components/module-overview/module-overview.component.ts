c7f96b89a97a1768419b0770dab49f16e15fb9da
Jacob Johnston <methodician@gmail.com>
2022-03-14 19:48:15 +0000

    Really nicely tweaked and optimized

--- src/app/components/module-overview/module-overview.component.ts
+++ src/app/components/module-overview/module-overview.component.ts
@@ -1,27 +1,16 @@
 import { Component } from '@angular/core';
 import { AngularFireDatabase } from '@angular/fire/compat/database';
 import { ActivatedRoute } from '@angular/router';
-import {
-  EChartsOption,
-  XAXisComponentOption,
-  YAXisComponentOption,
-  SeriesOption,
-} from 'echarts';
-import { combineLatest, first, map } from 'rxjs';
+import { EChartsOption, SeriesOption, LineSeriesOption } from 'echarts';
+import { filter, map, Subject, takeUntil } from 'rxjs';
 import { ModuleI, ReadoutI } from 'src/app/models';
 
 const baseChatOption: EChartsOption = {
-  // toolbox: {
-  //   right: 20,
-  //   feature: {
-  //     dataZoom: {
-  //       yAxisIndex: 'none',
-  //     },
-  //     saveAsImage: {},
-  //     restore: {},
-  //     magicType: {},
-  //   },
-  // },
+  title: {
+    text: 'Room Loading',
+    left: 'center',
+    top: 20,
+  },
   tooltip: {
     trigger: 'axis',
     axisPointer: {
@@ -42,12 +31,51 @@ const baseChatOption: EChartsOption = {
       },
     },
   },
-  yAxis: {
-    type: 'value',
-    name: 'y axis',
-    min: 12,
-  },
-  series: [],
+  yAxis: [
+    {
+      type: 'value',
+      name: 'humidity',
+      min: 25,
+    },
+    {
+      type: 'value',
+      name: 'temperature',
+      inverse: true,
+      position: 'left',
+    },
+    // {
+    //   type: 'value',
+    //   name: 'pressure',
+    // },
+    {
+      type: 'value',
+      name: 'gas',
+      position: 'right',
+      // offset: 60,
+    },
+  ],
+  series: [
+    {
+      name: 'humidity',
+      type: 'line',
+      yAxisIndex: 0,
+    },
+    {
+      name: 'temperature',
+      type: 'line',
+      yAxisIndex: 0,
+    },
+    // {
+    //   name: 'pressure',
+    //   type: 'line',
+    //   yAxisIndex: 1,
+    // },
+    {
+      name: 'gas',
+      type: 'line',
+      yAxisIndex: 2,
+    },
+  ],
   dataZoom: [
     {
       startValue: new Date().setHours(0),
@@ -62,99 +90,101 @@ const baseChatOption: EChartsOption = {
   styleUrls: ['./module-overview.component.scss'],
 })
 export class ModuleOverviewComponent {
+  private unsubscribe$ = new Subject<void>();
   //! querying is sooo bad with rtdb maybe I really should just go with firestore
   chartOption: EChartsOption = baseChatOption;
-  updateOption: EChartsOption;
+  updateOption: EChartsOption = {};
+  moduleId: string;
+
+  // options
+  shouldConvertToFahrenheit = true;
 
   constructor(
     private activeRoute: ActivatedRoute,
     private db: AngularFireDatabase,
   ) {
     this.initializeChart();
+    this.unsubscribe$.subscribe(() => console.log('unsubscribed'));
+  }
+
+  ngOnDestroy() {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
+  }
+
+  updateSeries(series: SeriesOption[]) {
+    this.updateOption = {
+      ...this.updateOption,
+      series,
+    };
+  }
+
+  updateTitle(title: EChartsOption['title']) {
+    //? why does this work when updateSeries has to keep object intact?
+    this.updateOption.title = title;
   }
 
   initializeChart = () => {
     this.activeRoute.params
       .pipe(
         map(params => params['id']),
-        first(id => !!id),
+        filter(id => !!id),
       )
       .subscribe(id => {
+        this.moduleId = id;
         const module$ = this.db.object<ModuleI>(`modules/${id}`).valueChanges();
+
+        module$
+          .pipe(
+            filter(module => !!module),
+            takeUntil(this.unsubscribe$),
+          )
+          .subscribe(module => {
+            const { name } = module!;
+            const title: EChartsOption['title'] = {
+              text: name,
+            };
+
+            this.updateTitle(title);
+          });
+
         const readouts$ = this.db
           .list<ReadoutI>('readouts', ref =>
             ref.orderByChild('moduleId').equalTo(id),
           )
           .valueChanges();
-        combineLatest([module$, readouts$]).subscribe(([module, readouts]) => {
-          if (!readouts) {
-            return;
-          }
-          const xAxis: XAXisComponentOption = {
-            type: 'time',
-            alignTicks: true,
-            axisLabel: {
-              formatter: function (value: number) {
-                let date = new Date(value);
-                if (date.getHours() === 0) {
-                  return date.toLocaleDateString();
-                }
 
-                return date.toLocaleTimeString();
+        readouts$
+          .pipe(
+            filter(readouts => !!readouts),
+            takeUntil(this.unsubscribe$),
+          )
+          .subscribe(readouts => {
+            const series: LineSeriesOption[] = [
+              {
+                data: readouts.map(readout => [
+                  readout.timestamp,
+                  readout.bme.humidity,
+                ]),
               },
-            },
-          };
-          const humidity = readouts.map(readout => [
-            readout.timestamp,
-            readout.bme.humidity,
-          ]);
-          const temperature = readouts.map(readout => [
-            readout.timestamp,
-            readout.bme.temperature,
-          ]);
-          const yAxisHumidity: YAXisComponentOption = {
-            type: 'value',
-            name: 'Humidity',
-          };
-          const yAxisTemperature: YAXisComponentOption = {
-            name: 'Temperature',
-            alignTicks: true,
-            type: 'value',
-          };
-          const yAxis: YAXisComponentOption[] = [
-            yAxisHumidity,
-            yAxisTemperature,
-          ];
-          const humiditySeries: SeriesOption = {
-            name: 'Humidity',
-            type: 'line',
-            data: humidity,
-          };
-          const temperatureSeries: SeriesOption = {
-            name: 'Temperature',
-            type: 'line',
-            data: temperature,
-            yAxisIndex: 1,
-          };
-          const series: SeriesOption[] = [humiditySeries, temperatureSeries];
-          const chartOption: EChartsOption = {
-            title: {
-              text: `${module?.name} - Humidity vs Temperature`,
-              left: 'center',
-              top: 20,
-            },
-            tooltip: {
-              trigger: 'axis',
-              axisPointer: {
-                type: 'cross',
+              {
+                data: readouts.map(readout => [
+                  readout.timestamp,
+                  this.shouldConvertToFahrenheit
+                    ? (readout.bme.temperature * 9) / 5 + 32
+                    : readout.bme.temperature,
+                ]),
               },
-            },
-            yAxis,
-            xAxis,
-            series,
-          };
-          this.updateOption = chartOption;
-        });
+              {
+                data: readouts.map(readout => [
+                  readout.timestamp,
+                  readout.bme.gas,
+                ]),
+              },
+            ];
+
+            this.updateSeries(series);
+          });
       });
   };
 }
